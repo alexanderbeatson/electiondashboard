f2fa672987c2a57bf336f8eac7cf796473163feb
Alexander Beatson <saibohne@phandeeyar.org>
2019-05-15 16:22:42 +0630

    drop down works

--- byConst.js
+++ byConst.js
@@ -1,18 +1,25 @@
-function buildDropdowns {
+function buildDropdowns(){
   var years = Object.keys(elect);
   var houses = Object.keys(elect[years[0]]);
 
-  var dropdownDiv = d3.select('.dropdown')
-    .append('select')
+  var dropdownDiv = d3.select('.dropdown');
+  
+  dropdownDiv.append('select')
     .attr('class', 'year')
     .attr('name', 'year')
     .selectAll('option')
     .data(years)
     .enter()
     .append('option')
-    .value(function(d) { return d;})
-    .text(function(d) { return d;});
-
+    .attr('value', function(d) { return d;})
+    .text(function(d) { return d.slice(2);});
+  
+  var houseNameTranslate = {
+      'amyotha' : 'Amyotha',
+      'pyithu' : 'Pyithu',
+      'sr' : 'State/Region'
+  };
+    
   dropdownDiv.append('select')
     .attr('class', 'house')
     .attr('name', 'house')
@@ -20,6 +27,6 @@ function buildDropdowns {
     .data(houses)
     .enter()
     .append('option')
-    .value(function(d) { return d;})
-    .text(function(d) { return d;});
+    .attr('value', function(d) { return d;})
+    .text(function(d) { return houseNameTranslate[d];});
 }
